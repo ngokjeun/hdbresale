07eb1707e9b36180b0462bff2243376e92c474d9
ngokjeun <n.jeunw@gmail.com>
2022-11-03 19:39:45 +0800

    added random forest regressor

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from sklearn.linear_model import LinearRegression
 from streamlit_option_menu import option_menu
 import datetime as dt
 from sklearn import metrics
+from sklearn.ensemble import RandomForestRegressor
 
 
 st.set_page_config(page_title="HDB Resale Prices",
@@ -55,9 +56,6 @@ x = ['lease_commence_date', 'flat_kind', 'sale_date', 'town_ANG MO KIO', 'town_B
 x = df_town[x]
 y = df_town["resale_price"]
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-lr = LinearRegression()
-lr.fit(X_train, y_train)
-# coeff_df = pd.DataFrame(lr.coef_,x.columns, columns = ["Coefficient"])
 
 
 # -- sidebar --
@@ -165,7 +163,7 @@ if selected == "home":
 
     # -- prediction --
     st.title("HDB Resale Price Prediction")
-    st.write("This section is independent of sidebar selections. Price is calculated with a linear regression model.")
+    st.write("This section is independent of sidebar selections. Price is calculated with regression models.")
     st.subheader("Flat Type")
     flat_select = st.slider("Number of rooms, 6 for Executive and 7 for Multi-Gen", value=4, min_value=1, max_value=7, step=1)
     st.subheader("Lease Commence Year")
@@ -175,6 +173,9 @@ if selected == "home":
         key = "not_sidebar"
         )
     date_select = st.text_input("Key in the date: i.e. YYYY-MM", "2022-09")
+    regression_select = st.selectbox("Select the regression method: (Linear is faster, Random Forest is more accurate)",
+        options=["Linear Regressor", "Random Forest Regressor"]
+        )
 
     #converting user inputs
     ordinal_date = dt.datetime.toordinal(pd.to_datetime(date_select, format='%Y-%m'))
@@ -184,8 +185,16 @@ if selected == "home":
              t_index = df_town.columns.get_loc(i) - 14
              town_select_list = town_select_list[:t_index]+[1]+town_select_list[t_index+1:]
     user_list = [year_select, flat_select, ordinal_date] + town_select_list
-
-    price_predicted = lr.predict([user_list])[0]
+    
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+    
+    if regression_select == "Linear Regressor":
+        price_predicted = lr.predict([user_list])[0]
+    elif regression_select == "Random Forest Regressor":
+        rfr = RandomForestRegressor(min_samples_split=3, n_estimators=20, min_samples_leaf = 1)
+        rfr.fit(X_train, y_train)
+        price_predicted = rfr.predict([user_list])[0]
 
     html_str = f"""
     <style>
@@ -201,10 +210,10 @@ if selected == "home":
 
 if selected == "the model":
     st.title("the model")
-    st.markdown("**A simple scikit linear regression model was used in this demonstration**")
+    st.markdown("**Scikit linear and random forest regression models were used in this demonstration**")
     st.write("To include town as a variable, I represented it as a dummy variable.")
     st.code("df_town = pd.concat([df,pd.get_dummies(df.town, prefix='town')],axis=1)")
-    st.write("Converting user inputs was the most challenging part, as I have yet to understand labeling of nominal categorical variables after encoding(i.e. town). Hence I populated a list of 0s and 1s with a for loop.")
+    st.write("Converting user inputs was a challenge, as I have yet to understand labeling of nominal categorical variables after encoding(i.e. town). Hence I populated a list of 0s and 1s with a for loop.")
     st.code("""
     town_select_list = [0] * 26
     for i in df_town.columns:
@@ -223,38 +232,63 @@ if selected == "the model":
     y = df_town["resale_price"]
     ''')
     st.write("Splitting the data into training and testing sets")
-    st.code("X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)")
+    st.code("X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42) #this is used for both linear and random forest regressors")
     st.write("Creating a linear regression object with scikit")
     st.code("lr = LinearRegression()")
+    st.code("rfr = RandomForestRegressor() #parameters can be added to increase speed, I used min_samples_split=3, n_estimators=20, min_samples_leaf = 1")
     st.write("Train the model using sets specified")
     st.code("lr.fit(X_train, y_train)")
-    st.write("The coefficients")
+    st.code("rfr.fit(X_train, y_train)")
+    st.write("The coefficients from linear regression")
     st.code('coeff_df = pd.DataFrame(lr.coef_,x.columns, columns = ["Coefficient"])')
     lr = LinearRegression()
     lr.fit(X_train, y_train)
     coeff_df = pd.DataFrame(lr.coef_,x.columns, columns = ["Coefficient"])
     st.dataframe(coeff_df)
+    rfr = RandomForestRegressor(min_samples_split=3, n_estimators=20, min_samples_leaf = 1)
+    rfr.fit(X_train, y_train)
     st.write("Predict for test set")
     st.code("predictions = lr.predict(X_test)")
-    st.write("Plot predictions against actual")
+    st.code("predictions = rfr.predict(X_test)")
+    st.markdown("***Plot predictions against actual***")
     st.code("px.scatter(y_test, predictions)")
+    st.code("px.scatter(y_test, predictions_rfr)")
     predictions = lr.predict(X_test)
+    predictions_rfr = rfr.predict(X_test)
     fig_scatter = px.scatter(x=y_test, y=predictions)
-    st.plotly_chart(fig_scatter)
-    st.write("As the model becomes more accurate, we would expect a upwards-sloping, linear line. In the previous version of this model, the flat area was used as a variable. However, since flat area is dependent on flat type (95% correlated), it was not an independent variable, hence was removed from the model.")
-    st.write("Plot prediction distribution")
+    fig_scatter_rfr = px.scatter(x=y_test, y=predictions_rfr)
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.write("Linear Regression")
+        st.plotly_chart(fig_scatter, use_container_width=True)
+    with right_column:
+        st.write("Random Forest Regression")
+        st.plotly_chart(fig_scatter_rfr, use_container_width=True)
+    st.write("As observed, the random forest regressor produced a more compact, linear 'line'. This is indicative of a more accurate model. In the previous version of this model, the flat area was used as a variable. However, since flat area is dependent on flat type (95% correlated), it was not an independent variable, hence was removed from the model.")
+    st.markdown("***Plot prediction distribution***")
     st.code("px.histogram((y_test-predictions))")
-    fig_hist = px.histogram((y_test-predictions))
-    st.plotly_chart(fig_hist)
-    st.write("This distribution graph is in the shape we expect. As the model becomes more accurate, the spread becomes tighter.")
+    st.code("px.histogram((y_test-predictions_rfr))")
+    left_column2, right_column2 = st.columns(2)
+    with left_column2:
+        st.write("Linear Regression")
+        fig_hist = px.histogram((y_test-predictions))
+        st.plotly_chart(fig_hist, use_container_width=True)
+    with right_column2:    
+        st.write("Random Forest Regression")
+        fig_hist_rfr = px.histogram((y_test-predictions_rfr))
+        st.plotly_chart(fig_hist_rfr, use_container_width=True)
+    st.write("This distribution graph is in the bell shape we expect. As the model becomes more accurate, the spread becomes tighter. We can see that the distribution of predictions for the random forest regressor is significantly tighter than the linear regression model.")
     st.write("To find the R^2 value, we use the score function")
     st.code("metrics.r2_score(y_test, predictions)")
-    st.write(metrics.r2_score(y_test, predictions))
-    st.write("The R^2 value is around 0.8, which is decent for a linear regressor but not good enough for production. It can be improved with another model, like a Random Forest Regressor, or by including more variables")
+    st.code("metrics.r2_score(y_test, predictions_rfr)")
+    st.write(metrics.r2_score(y_test, predictions), metrics.r2_score(y_test, predictions_rfr))
+    st.write("The R^2 value is around 0.8, which is decent for a linear regressor. The R^2 value of around 0.9 of the random forest regressor is improved")
     st.write("To find the Mean Absolute Error, we use the mean_absolute_error function")
     st.code("metrics.mean_absolute_error(y_test, predictions)")
-    st.write(metrics.mean_absolute_error(y_test, predictions))
-    st.write("The Mean Absolute Error shows that the model is off by around $57,000 on average.")
+    st.code("metrics.mean_absolute_error(y_test, predictions_rfr)")
+    st.write(metrics.mean_absolute_error(y_test, predictions), metrics.mean_absolute_error(y_test, predictions_rfr))
+    st.markdown("The Mean Absolute Error shows that the linear regression model is off by around $57,000 on average.")
+    st.markdown("In contrast, the random forest regressor is off by around $34,000 on average. This is a significant improvement.")
 
 
 
